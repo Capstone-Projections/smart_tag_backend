912abd55ef0a9cacc3a46de7f57b0caa2ed4ab6c
kwabendarkwa <darkwak@live.com>
2023-06-01 10:17:41 +0000

    :tada: Seed a many to many relationship successfully
    This commit represents the point where I was able to successfully seed the tables of a many to many relationship

--- src/seed.ts
+++ src/seed.ts
@@ -5,12 +5,49 @@ const prisma = new PrismaClient()
 
 // A `main` function so that we can use async/await
 async function main() {
-    const kwabena = await prisma.student.create({
-        data:{
-            email: "kwabena@live.com",
+    // const kwabena = await prisma.student.create({
+    //     data:{
+    //         email: "kdobengyeboah@st.knust.edu.gh",
+    //         password: "Agent99.99",
+    //         firstName: "Kwabena",
+    //         middleName: "Darkwa",
+    //         lastName: "Obeng-Yeboah",
+    //         referenceNumber: "20675730",
+    //         indexNumber: 8262719,
+    //         studyProgram: "Computer Engineering",
+    //         doubtPoints: 0.0,
+    //         attendance: {
+
+    //         },
+
+
+
+    //     }})
+    const akowuah = await prisma.lecturer.create({
+        data: {
+            email: "darkwak@live.com",
             firstName: "Kwabena",
-        }
-    })
+            lastName: "Darkwa",
+            password: "Agent99.99",
+            title: "Mr",
+            staffID: "STF/0001",
+            course_has_lecturer:{
+                create:{
+                    course:{
+                        connect:{
+                            idcourse:1
+                        }
+                    }
+
+                }
+            
+            }
+            }
+    
+
+})
+
+
 }
 
 main()
@@ -22,3 +59,6 @@ main()
     // Disconnect Prisma Client
     await prisma.$disconnect()
     })
+
+
+
