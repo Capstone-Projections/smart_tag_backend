927bc2e5f1836294c7d105bd5da7bcce74362a73
kwabendarkwa <darkwak@live.com>
2023-06-03 13:19:36 +0000

    :rotating_light: Fix linting issues with prettier

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,24 @@
-import Hapi from '@hapi/hapi'
+import Hapi from '@hapi/hapi';
 
 const server: Hapi.Server = Hapi.server({
-  port: process.env.PORT || 3000,
-  host: process.env.HOST || 'localhost',
-})
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'localhost',
+});
 
 export async function start(): Promise<Hapi.Server> {
-  await server.start()
-  return server
+    await server.start();
+    return server;
 }
 
-process.on('unhandledRejection', err => {
-  console.log(err)
-  process.exit(1)
-})
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
 
 start()
-  .then(server => {
-    console.log(`Server running on ${server.info.uri}`)
-  })
-  .catch(err => {
-    console.log(err)
-  })
+    .then((server) => {
+        console.log(`Server running on ${server.info.uri}`);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
