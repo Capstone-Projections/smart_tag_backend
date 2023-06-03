5f4526d8a95b53b9678c715757decb8b1abd1e94
kwabendarkwa <darkwak@live.com>
2023-06-03 13:17:53 +0000

    :heavy_plus_sign: Include hapi to project and include server.ts file

--- src/seed.ts
+++ src/seed.ts
@@ -25,97 +25,95 @@ async function main() {
     });
 
     const lectureRoom = await prisma.lectureroom.createMany({
-        data:[
+        data: [
             {
                 name: 'A110',
-                roomLocation:'Ceasar Building',
+                roomLocation: 'Ceasar Building',
                 uid: '123',
             },
             {
                 name: '302',
-                roomLocation:'Bamfo Kwakye Building',
+                roomLocation: 'Bamfo Kwakye Building',
                 uid: '124',
             },
             {
                 name: '303',
-                roomLocation:'Bamfo Kwakye Building',
+                roomLocation: 'Bamfo Kwakye Building',
                 uid: '125',
             },
-
-        ]
+        ],
     });
 
     const lesson = await prisma.lesson.create({
         data: {
-            course_has_lesson:{
-                create:{
-                    course:{
-                        connect:{
-                            idcourse:1
-                        }
-                    }
-                }
+            course_has_lesson: {
+                create: {
+                    course: {
+                        connect: {
+                            idcourse: 1,
+                        },
+                    },
+                },
             },
-            lectureroom:{
-                connect:{
-                    idlectureRoom:1
-                }
+            lectureroom: {
+                connect: {
+                    idlectureRoom: 1,
+                },
             },
-            day: "Tuesday",
-            startTime: "10:30",
-            endTime: "12:30",
+            day: 'Tuesday',
+            startTime: '10:30',
+            endTime: '12:30',
         },
     });
 
     const attendance = await prisma.attendance.create({
-        data:{
-            status:true,
-            student:{
-                connect:{
-                    idstudent:1
-                }
+        data: {
+            status: true,
+            student: {
+                connect: {
+                    idstudent: 1,
+                },
             },
-            lesson:{
-                connect:{
-                    idlesson:2
-                }
-            }
-
+            lesson: {
+                connect: {
+                    idlesson: 2,
+                },
+            },
         },
     });
 
     const moses = await prisma.student.create({
-            data:{
-                email: "baffourma@st.knust.edu.gh",
-                password: "Agent99.99",
-                firstName: "Moses",
-                middleName: "Awuah",
-                lastName: "Baffour",
-                referenceNumber: "20575730",
-                indexNumber: 8263719,
-                studyProgram: "Computer Engineering",
-                doubtPoints: 0.0,
-                attendance: {
-                        create:{
-                            status:true,
-                            lesson:{
-                                connect:{
-                                    idlesson:2
-                                }
-                            },
-                        }
+        data: {
+            email: 'baffourma@st.knust.edu.gh',
+            password: 'Agent99.99',
+            firstName: 'Moses',
+            middleName: 'Awuah',
+            lastName: 'Baffour',
+            referenceNumber: '20575730',
+            indexNumber: 8263719,
+            studyProgram: 'Computer Engineering',
+            doubtPoints: 0.0,
+            attendance: {
+                create: {
+                    status: true,
+                    lesson: {
+                        connect: {
+                            idlesson: 2,
+                        },
+                    },
                 },
-                student_has_course:{
-                    create:{
-                        course:{
-                            connect:{
-                                idcourse:1
-                            }
-                        }
-                    }
-                }
-
-            }})
+            },
+            student_has_course: {
+                create: {
+                    course: {
+                        connect: {
+                            idcourse: 1,
+                        },
+                    },
+                },
+            },
+        },
+    });
 
     const course = await prisma.course.create({
         data: {
