5233f88bf3ae26f40dd57e259cbd97b5d968fcda
kwabendarkwa <darkwak@live.com>
2023-06-03 14:52:26 +0000

    :sparkles: Set up server and inldue one endpoint

--- src/configs/server.ts
+++ src/configs/server.ts
@@ -1,4 +1,5 @@
 import Hapi from '@hapi/hapi';
+import status from '../plugins/status'
 
 const server: Hapi.Server = Hapi.server({
     port: process.env.PORT || 3000,
@@ -6,8 +7,10 @@ const server: Hapi.Server = Hapi.server({
 });
 
 export async function start(): Promise<Hapi.Server> {
-    await server.start();
-    return server;
+    await server.register([status])
+    await server.start()
+    console.log(`Server running on ${server.info.uri}`)
+    return server
 }
 
 process.on('unhandledRejection', (err) => {
