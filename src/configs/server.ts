aee536a930d996572802a6971d72d90e816f78de
kwabendarkwa <darkwak@live.com>
2023-06-03 23:44:19 +0000

    :wrench: Add prisma.ts file to ensure that all routes are wrapped with it

--- src/configs/server.ts
+++ src/configs/server.ts
@@ -1,5 +1,7 @@
 import Hapi from '@hapi/hapi';
 import status from '../plugins/status';
+import prismaPlugin from '../plugins/prisma';
+
 
 const server: Hapi.Server = Hapi.server({
     port: process.env.PORT || 3000,
@@ -7,7 +9,7 @@ const server: Hapi.Server = Hapi.server({
 });
 
 export async function createServer(): Promise<Hapi.Server> {
-    await server.register([status]);
+    await server.register([status,prismaPlugin]);
     await server.initialize();
     return server;
 }
