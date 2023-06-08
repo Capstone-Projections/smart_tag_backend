69844c70cc4ebba5058c7e3e5dd6b69aeb836903
kwabendarkwa <darkwak@live.com>
2023-06-08 10:33:32 +0000

    :sparkles: Finalize on the email token message
    This is the push that finalizes the email token look when the email is sent to a user. The naming might be changed but this seems like the final   look

--- src/plugins/email.ts
+++ src/plugins/email.ts
@@ -22,8 +22,6 @@ const emailPlugin = {
             server.app.sendEmailToken = debugSendEmailToken;
         } else {
             sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
-            // sendgrid.setApiKey("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");
-            server.log(`hey server	`);
             server.app.sendEmailToken = sendEmailToken;
         }
     },
@@ -40,70 +38,124 @@ async function sendEmailToken(email: string, token: string) {
         subject: 'Login token for Smart Tag Account',
         text: `The login token for the API is: ${token}`,
         html: `
-            <html>
-                <head>
-                    <style>
-                        .container {
-                            background-color: ${backgroundColor};
-                            padding: 20px;
-                            border-radius: 5px;
-                            color: #333;
-                            font-family: Arial, sans-serif;
-                            max-width: 600px;
-                            margin: 0 auto;
-                        }
-
-                        h1 {
-                            font-size: 24px;
-                            margin-bottom: 20px;
-                        }
-
-                        p {
-                            font-size: 16px;
-                            margin-bottom: 10px;
-                        }
-
-                        .token {
-                            font-weight: bold;
-                            font-size: 20px;
-                        }
-
-                        .attendance-info {
-                            margin-top: 30px;
-                            border-top: 1px solid #333;
-                            padding-top: 20px;
-                        }
-
-                        .info-title {
-                            font-size: 18px;
-                            font-weight: bold;
-                            margin-bottom: 10px;
-                        }
-
-                        .info-description {
-                            font-size: 16px;
-                        }
-                    </style>
-                </head>
-                <body>
-                    <div class="container">
-                        <h1>Login token for Smart Tag Account</h1>
-                        <p>Your token is:</p>
-                        <p class="token">${token}</p>
-                        <div class="attendance-info">
-                            <h2 class="info-title">Smart Tag</h2>
-                            <p class="info-description">Thank you for using our attendance-taking application. With this token, you can securely access the system and manage attendance records.</p>
-                            <p class="info-description">Please keep this token confidential and do not share it with others.</p>
-                        </div>
-                    </div>
-                </body>
-            </html>
-        `,
+        <html>
+        <head>
+          <style>
+            .container {
+              background-color: #f0f0f0;
+              padding: 20px;
+              border-radius: 5px;
+              color: #333;
+              font-family: Arial, sans-serif;
+              max-width: 600px;
+              margin: 0 auto;
+            }
+      
+            h1 {
+              font-size: 24px;
+              margin-bottom: 20px;
+            }
+      
+            p {
+              font-size: 16px;
+              margin-bottom: 10px;
+            }
+      
+            .token {
+              font-weight: bold;
+              font-size: 20px;
+              cursor: pointer;
+              user-select: all;
+            }
+      
+            .attendance-info {
+              margin-top: 30px;
+              border-top: 1px solid #333;
+              padding-top: 20px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+      
+            .nfc-sign {
+              position: relative;
+              width: 150px;
+              height: 150px;
+              background-color: #0e4b7d;
+              border-radius: 50%;
+              overflow: hidden;
+            }
+      
+            .nfc-sign:before,
+            .nfc-sign:after {
+              content: "";
+              position: absolute;
+              width: 40px;
+              height: 40px;
+              background-color: #f2d40f;
+              border-radius: 50%;
+            }
+      
+            .nfc-sign:before {
+              top: 35px;
+              left: 55px;
+            }
+      
+            .nfc-sign:after {
+              top: 75px;
+              left: 15px;
+            }
+      
+            .nfc-sign .logo {
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+              width: 80px;
+              height: 80px;
+              background-color: #000;
+              border-radius: 50%;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+      
+            .nfc-sign .logo:before {
+              content: "";
+              position: absolute;
+              width: 50px;
+              height: 50px;
+              background-color: #f2d40f;
+              border-radius: 50%;
+            }
+      
+            .nfc-sign .logo:after {
+              content: "";
+              position: absolute;
+              width: 20px;
+              height: 20px;
+              background-color: #0e4b7d;
+              border-radius: 50%;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>Smart Tag</h1>
+            <p>Your token is:</p>
+            <p class="token" id="token" onclick="copyToken()">${token}</p>
+            <div class="attendance-info">
+              <div class="nfc-sign">
+                <div class="logo"></div>
+              </div>
+            </div>
+          </div>
+        </body>
+      </html>        `,
     };
 
     await sendgrid.send(msg);
 }
-
 async function debugSendEmailToken(email: string, token: string) {
     console.log(`email token for ${email}: ${token} `);
 }
